e5a4c4aa3aa8362c4948f58e7a408d9a669feb5e
rizrusn <rizki.nuryadin@lippox.com>
2016-08-09 11:43:38 +0700

    get all and create

---
+++ client.php
@@ -0,0 +1,49 @@
+<?php
+//include "connect.php";
+
+	$verb = $_SERVER['REQUEST_METHOD'];
+
+	if ($verb == 'GET'){
+		getAll();
+	}elseif ($verb == 'POST') {
+		create();
+	}elseif ($verb == 'DELETE'){
+
+	}elseif ($verb == 'PUT'){
+
+	}
+
+	function create(){
+		$conn = mysqli_connect("localhost","root","","api_test") or die("koneksi gagal");
+
+		$vname = $_POST['name'];
+		$vgender = $_POST['gender'];
+
+		$status = mysqli_query($conn,"INSERT INTO clients (name,gender) VALUES ('$vname', '$vgender')")
+			or die("Error: ".mysqli_error($conn));
+
+		$id = mysqli_insert_id($conn);
+		$array = array('id'=>$id);
+	    
+	    $json = json_encode($array);
+	    return echo $json;
+
+	    close($conn);
+	}
+
+	function getAll(){
+		$conn = mysqli_connect("localhost","root","","api_test") or die("koneksi gagal");
+		$data = mysqli_query($conn,"select * from clients")
+			or die("Error: ".mysqli_error($conn));
+		$clients = array();
+		while ($client = mysqli_fetch_array($data) {
+			$clients[] = $client;
+		}
+		$clients = json_encode($clients);
+		return $clients;
+
+		close($conn);
+	}
+	
+
+?>
